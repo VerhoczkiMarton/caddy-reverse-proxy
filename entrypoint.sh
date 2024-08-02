2b95b4189559b4e57daeb615f0ddd3e22670972a
VerhoczkiMarton <verhoczki.marci@gmail.com>
2024-08-02 20:18:29 +0200

    Remove backwards compability

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,11 @@
-#!/bin/sh
-
+#!/bin/bash
 set -euo pipefail
 
-# for backwards compatibility, seperates host and port from url
-export FRONTEND_DOMAIN=${FRONTEND_DOMAIN:-${FRONTEND_HOST%:*}}
-export FRONTEND_PORT=${FRONTEND_PORT:-${FRONTEND_HOST##*:}}
-
-export BACKEND_DOMAIN=${BACKEND_DOMAIN:-${BACKEND_HOST%:*}}
-export BACKEND_PORT=${BACKEND_PORT:-${BACKEND_HOST##*:}}
-
-# strip https:// or https:// from domain if necessary
+# Strip http:// or https:// from domains if necessary
 FRONTEND_DOMAIN=${FRONTEND_DOMAIN##*://}
 BACKEND_DOMAIN=${BACKEND_DOMAIN##*://}
 
-echo using frontend: ${FRONTEND_DOMAIN} with port: ${FRONTEND_PORT}
-echo using backend: ${BACKEND_DOMAIN} with port: ${BACKEND_PORT}
+echo "Using frontend: ${FRONTEND_DOMAIN} with port: ${FRONTEND_PORT}"
+echo "Using backend: ${BACKEND_DOMAIN} with port: ${BACKEND_PORT}"
 
 exec caddy run --config Caddyfile --adapter caddyfile 2>&1
